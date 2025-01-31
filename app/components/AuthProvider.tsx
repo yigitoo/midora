97066a596627dca4b05c4c3e6246164bf10b2acf
Yiğit GÜMÜŞ <gumusyigit101@gmail.com>
2025-02-01 01:10:46 +0300

    update the remote

--- app/components/AuthProvider.tsx
+++ app/components/AuthProvider.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useRouter } from "next/router"
 import type React from "react"
 import { createContext, useContext, useState } from "react"
 
@@ -12,10 +13,11 @@ type AuthContextType = {
 const AuthContext = createContext<AuthContextType | undefined>(undefined)
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const router = useRouter()
   const [isLoggedIn, setIsLoggedIn] = useState(false)
 
   const login = () => setIsLoggedIn(true)
-  const logout = () => setIsLoggedIn(false)
+  const logout = () => {setIsLoggedIn(false); router.push('/')}
 
   return <AuthContext.Provider value={{ isLoggedIn, login, logout }}>{children}</AuthContext.Provider>
 }
