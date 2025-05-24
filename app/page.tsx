52ee3ee6883b1e03fc2037371c5423019d18c59c
Yiğit GÜMÜŞ <gumusyigit101@gmail.com>
2025-05-24 18:24:56 +0300

    push

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { CTA } from "@/components/sections/cta"
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen rounded-2xl">
       <Hero />
       <Features />
       <Stats />
